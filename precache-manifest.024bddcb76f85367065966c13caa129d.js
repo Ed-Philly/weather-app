829d7937e5b711a8bd4ccfb15e0aed358a0c2e7a
Ed philly <osas.edok@yahoo.co.uk>
2020-05-26 02:33:16 +0100

    Updates

---
+++ precache-manifest.024bddcb76f85367065966c13caa129d.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80a1ebe27e76b80711e192a223b309db",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "235e7aee45ec573ee4bb",
+    "url": "/weather-app/static/css/main.93b54e48.chunk.css"
+  },
+  {
+    "revision": "f777e3168b1aac297de5",
+    "url": "/weather-app/static/js/2.2dbb65ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-app/static/js/2.2dbb65ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "235e7aee45ec573ee4bb",
+    "url": "/weather-app/static/js/main.5216155c.chunk.js"
+  },
+  {
+    "revision": "cfd878ec1ba460d7402a",
+    "url": "/weather-app/static/js/runtime-main.af702457.js"
+  },
+  {
+    "revision": "d263b0e0f4b554f12ee1b0c9d475bf97",
+    "url": "/weather-app/static/media/italy-bg-1.d263b0e0.jpeg"
+  },
+  {
+    "revision": "6ccd31478cb3bb0f4080f93570f664ce",
+    "url": "/weather-app/static/media/italy-bg-2.6ccd3147.jpeg"
+  },
+  {
+    "revision": "535f3b0302c54bc76f71e0c797899786",
+    "url": "/weather-app/static/media/italy-bg-3.535f3b03.jpeg"
+  },
+  {
+    "revision": "beb210aa212844fcd4683d3f48d4fd40",
+    "url": "/weather-app/static/media/italy-bg-5.beb210aa.jpeg"
+  },
+  {
+    "revision": "14d86749ccf141dbaf4d246d4d49554b",
+    "url": "/weather-app/static/media/london-bg-1.14d86749.jpeg"
+  },
+  {
+    "revision": "02b47bccabda769467616a36c79c5e43",
+    "url": "/weather-app/static/media/london-bg-2.02b47bcc.jpeg"
+  },
+  {
+    "revision": "db0f8c08c9349a48b8494921ffbefde5",
+    "url": "/weather-app/static/media/london-bg-3.db0f8c08.jpeg"
+  },
+  {
+    "revision": "484570427a7941b41ea3b140f0dac575",
+    "url": "/weather-app/static/media/london-bg-4.48457042.jpeg"
+  },
+  {
+    "revision": "453dbe2a946ac6f8badbc6b48b7d7a12",
+    "url": "/weather-app/static/media/london-bg-5.453dbe2a.jpeg"
+  },
+  {
+    "revision": "18dfe309c3e956aafbac3129530c8605",
+    "url": "/weather-app/static/media/london-bg-6.18dfe309.jpeg"
+  },
+  {
+    "revision": "79efb373e6dfa851f64e2447dad323cd",
+    "url": "/weather-app/static/media/london-bg-7.79efb373.jpeg"
+  },
+  {
+    "revision": "a73e76ea7e271a2f13db394e579ff636",
+    "url": "/weather-app/static/media/london-bg-8.a73e76ea.jpeg"
+  },
+  {
+    "revision": "4eb79e095d927a0e640c951077f05263",
+    "url": "/weather-app/static/media/london-bg-9.4eb79e09.jpeg"
+  },
+  {
+    "revision": "e3c9cb3be993c5022b36b40a754bc703",
+    "url": "/weather-app/static/media/paris-bg-1.e3c9cb3b.jpeg"
+  },
+  {
+    "revision": "4d65161751f19814f7f3b74b7deb04d3",
+    "url": "/weather-app/static/media/paris-bg-2.4d651617.jpeg"
+  },
+  {
+    "revision": "a8d0137cb30f17115a1ca24a54ece4dd",
+    "url": "/weather-app/static/media/paris-bg-3.a8d0137c.jpeg"
+  },
+  {
+    "revision": "6f3b01bc9c38100a2ce378da7117d00e",
+    "url": "/weather-app/static/media/paris-bg-4.6f3b01bc.jpeg"
+  },
+  {
+    "revision": "984d52a9f1325d025e8fa970026e8acf",
+    "url": "/weather-app/static/media/paris-bg-5.984d52a9.jpeg"
+  },
+  {
+    "revision": "fb8b95b4cf230b4739f6610ae86c0a8c",
+    "url": "/weather-app/static/media/paris-bg-6.fb8b95b4.jpeg"
+  },
+  {
+    "revision": "80a65e067b16d6f692f9f40992b763f3",
+    "url": "/weather-app/static/media/paris-bg-7.80a65e06.jpeg"
+  },
+  {
+    "revision": "7da9133fa2ab6b37200303a46f098f6c",
+    "url": "/weather-app/static/media/rome-bg-1.7da9133f.jpeg"
+  },
+  {
+    "revision": "672d20a56712e82e012e1e3174df22a7",
+    "url": "/weather-app/static/media/rome-bg-2.672d20a5.jpeg"
+  },
+  {
+    "revision": "719b596aecb488885783975392e9a9ca",
+    "url": "/weather-app/static/media/rome-bg-3.719b596a.jpeg"
+  },
+  {
+    "revision": "cee77e30c558bc803245e4dded30a80f",
+    "url": "/weather-app/static/media/rome-bg-4.cee77e30.jpeg"
+  },
+  {
+    "revision": "61129a4d744a29e84c171187a12baa0e",
+    "url": "/weather-app/static/media/rome-bg-5.61129a4d.jpeg"
+  },
+  {
+    "revision": "d949853e211d087a8987eece10cc8913",
+    "url": "/weather-app/static/media/rome-bg-7.d949853e.jpeg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
